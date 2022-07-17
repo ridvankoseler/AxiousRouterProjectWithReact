8645b0fbc83b1b28500d750608296789d8bf07e4
ridvankoseler <ridvankoseler@gmail.com>
2022-07-18 00:25:41 +0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,41 @@
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import Followers from "../components/Followers"
+import SearchUser from '../components/SearchUser'
+
+const Home = () => {
+  const [allFollowers, setAllFollowers] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [search, setSearch] = useState("")
+
+  const getFollowers = async()=>{
+    const { data } = await axios.get(
+      "https://api.github.com/users/ridvankoseler/followers?per_page=100"
+    );
+    setAllFollowers(data)
+    console.log(data)
+  }
+
+  const handleChange=(e)=> {
+    setSearch(e.target.value)
+    console.log(e.target.value)
+  }
+
+  useEffect(() => {
+    getFollowers()
+    setTimeout(()=>{
+      setLoading(false)
+    },3000)
+  }, [])
+
+  const followersList = allFollowers.filter(follower=>follower.login.includes(search))
+  
+  return (
+    <div>
+      <SearchUser handleChange={handleChange}/>
+      <Followers followers={{loading,followersList}}/>
+    </div>
+  )
+}
+
+export default Home
